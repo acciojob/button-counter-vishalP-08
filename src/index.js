88f63481767aaf2f8625a74673c390044e568ff9
Vishal.AI <132605163+vishalP-08@users.noreply.github.com>
2023-12-16 17:50:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React, { useState } from 'react';
 
+const ClickCounter = () => {
+  const [clickCount, setClickCount] = useState(0);
 
+  const handleClick = () => {
+    setClickCount(clickCount + 1);
+  };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+  return (
+    <div>
+      <p>Button clicked {clickCount} times</p>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+};
+
+export default ClickCounter;
