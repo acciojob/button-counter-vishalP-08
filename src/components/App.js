f06687151dff76c98c3fc9170cf2e85aebe85168
Vishal.AI <132605163+vishalP-08@users.noreply.github.com>
2023-12-16 17:51:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 
 import React from "react";
 import './../styles/App.css';
+import ClickCounter from './ClickCounter';
+import ReactDOM from 'react-dom';
+
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+    <ClickCounter />
     </div>
   )
 }
 
-export default App
+ReactDOM.render(<App />, document.getElementById('root'));
